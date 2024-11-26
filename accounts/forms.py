b5e93fc930177ad6a942a7ba9b822c09138a6b67
unknown <milad1418@yahoo.com>
2024-11-27 00:45:56 +0330

    add crispy forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,9 +6,23 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         # fields = UserCreationForm.Meta.fields + ('age', 'email',)
         fields = ['username', 'email', 'first_name', 'last_name', 'age']
+        labels = {
+            'username': 'Enter Username :',
+            'email': 'Enter Email :',
+            'first_name': 'Enter First Name :',
+            'last_name': 'Enter Last Name :',
+            'age': 'Enter Age :',
+        }
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         # fields = UserChangeForm.Meta.fields
         fields = ['username', 'email', 'first_name', 'last_name', 'age']
+        labels = {
+            'username': 'Enter Username :',
+            'email': 'Enter Email :',
+            'first_name': 'Enter First Name :',
+            'last_name': 'Enter Last Name :',
+            'age': 'Enter Age :',
+        }
--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ from telnetlib import LOGOUT
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-ytv6ck_2je2r85va+fb1ax2lk%$3ot(b+1)epdne^6k&@s8=hb
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'pages',
+    'crispy_forms',  # pip install django-crispy-forms
 
 ]
 
@@ -73,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -83,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -103,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -114,7 +110,7 @@ TIME_ZONE = 'Asia/Tehran'
 USE_I18N = True
 
 USE_TZ = False
-#USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -127,7 +123,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# authentication Settings
 AUTH_USER_MODEL = 'accounts.CustomUser'
-
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms settings
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
