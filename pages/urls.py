e60df63e052a309b9ba474df2b3e65eb3f21688e
unknown <milad1418@yahoo.com>
2024-11-27 00:04:54 +0330

    add tests

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('pages/home/', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
 ]
