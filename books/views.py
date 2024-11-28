dcd1ab10764dfb8d684e7d3b782440eefbaf9d7e
unknown <milad1418@yahoo.com>
2024-11-28 23:16:25 +0330

    create coustom template tag filter. fuction to_lower for lowercase all words.

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import *
 
 # Create your views here.
@@ -11,3 +12,18 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'content', 'price']
+    template_name = 'books/book_create.html'
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'content', 'price']
+    template_name = 'books/book_update.html'
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list') # Redirect to Index page
